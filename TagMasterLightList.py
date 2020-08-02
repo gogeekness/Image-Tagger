75d6f11dcd599a9773831319f8c6e534110c7634
gogeekness <rdeseke@gmail.com>
2020-08-02 10:08:19 +0200

    Mostly working, if the image as 0th Exif data.  Need to add a blank 0th data to add tags to.

--- TagMasterLightList.py
+++ TagMasterLightList.py
@@ -4,7 +4,7 @@
 # A list of strings.  The strings are used at values and labels for the checkboxes
 TaggerList = ["armbinder","ponygirl","Outdoor","vet","art","two","nipple-clamps","two+","vibe", "tape",
               "zelda","princess","viking","saddle","beast", "computer", "leather bondage","box","blindfold",
-              "harness","fuzz","vintage","over_stuff","plug", "machine",,"cloud", "peach", "nylon"]
+              "harness","fuzz","vintage","over_stuff","plug", "machine","cloud", "peach", "nylon"]
 
-SpecialList = ["vet", "Fran", "viking", "benson", "Bishop", "house", "order", "xxx52", "hitomi",
-               "heather_mechel", "pet-tentacle", "coco", "zelda", "peach", "sneak", "polecat"]
+SpecialList = ["vet", "Fran", "viking", "benson", "Bishop", "house of Gord", "order", "xxx52", "hitomi",
+               "heather_mechel", "pet-tentacle", "coco", "zelda", "peach", "sneak", "polecat", "self"]
